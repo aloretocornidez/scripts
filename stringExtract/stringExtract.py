2a379e1b2e16558b5c45c059e7a8ffa9f98139f6
Alan Manuel <aloretocornidez@email.arizona.edu>
2022-03-10 22:01:31 -0700

    working script

--- stringExtract/stringExtract.py
+++ stringExtract/stringExtract.py
@@ -5,10 +5,79 @@
 import argparse
 import sys
 import fnmatch
+import os
 
 
+def stringExtract(text):
+    
 
-def stringExtract(fileName, text):
+    #names of the benchmarks and predictors
+    benchmarks = ['a2time01', 'bitmnp01', 'cacheb01', 'mcf', 'libquantum']
+    predictors = ['BiModeBP', 'LocalBP', 'MultiperspectivePerceptron8KB', 'MultiperspectivePerceptron64KB', 'TournamentBP']
+
+    #get the current working directory to open files.
+    cd = os.getcwd()
+
+    #set up a loop to read every benchmark and predictor.
+    for benchmark in benchmarks:
+        for predictor in predictors:
+            filename = cd + f"\homework2\Homework2_{predictor}_{benchmark}_StatsFile.txt"
+            print(f"Filename that will be opened: {filename}")
+            #open every file for every predictor.
+            f = open(filename, "r")
+
+
+            #Set up an array to write all the data that we need if it is found.
+            data = []
+            #flag if data is found.
+            flag = False
+
+            #read all of the lines in a file.
+            lines = f.readlines()
+
+            for line in lines:
+                
+                #% of banch misprediction rate
+                if "system.cpu.iew.branch_mispredict_rate" in line:
+                    flag = True
+                    data += [line]
+
+                #execution time
+                if "sim_seconds" in line:
+                    flag = True
+                    data += [line]
+
+                #instrucitons per clock
+                if "cpu.ipc" in line:
+                    flag = True
+                    data += [line]
+
+                #% of branches
+                if "system.cpu.iew.branch_percentage" in line:
+                    flag = True
+                    data += [line]
+
+            #close file
+            f.close()
+
+
+            #if data is found, append the data to the text file.
+            if flag == True:
+
+                #open the output to append
+                with open('outputHomework2Data.txt', 'a') as text_file:
+                    text_file.write(f"Predictor: {predictor}  Benchmark: {benchmark}\n")
+                    
+                    #write the list elements in the file.
+                    for datum in data:
+                        text_file.write(datum)
+                    
+                    #spacing for next entry.
+                    text_file.write("\n\n\n\n")
+
+            
+
+
 
 
     return
@@ -24,8 +93,8 @@ def getArgs():
     required = parser.add_argument_group("Required Aruments")
 
     #add each required arguemtns to the 'required' group.
-    required.add_argument('t', '--text', required=True, help = 'string to extract from file.')
-    required.add_argument('-f', '--fileName', required=True, help = 'path to the filename.')
+    required.add_argument('-t', '--text', required=True, help = 'string to extract from file.')
+    #required.add_argument('-f', '--fileName', required=True, help = 'path to the filename.')
     
 
     #prints help if no arguments are sent in.
@@ -39,13 +108,14 @@ def getArgs():
 
 
 def main():
-    #print("In main")
+
     args = getArgs()
     
+    text = args.text
 
 
     
-    stringExtract(text, filename)
+    stringExtract(text)
 
 
     
